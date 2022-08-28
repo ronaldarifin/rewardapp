cb40e451f7fbb9ce0cd11a4fa5a22215f4e6356b
ronaldarifin <71082114+ronaldarifin@users.noreply.github.com>
2022-08-28 12:37:48 -0700

    add routing for app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './routes/Home';
+import Configure from './routes/Configure';
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/configure" element={<Configure/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import App from "./App";
 import ReactDOM from 'react-dom/client';
 import React from "react";
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+     <App/>
   </React.StrictMode>
 );
