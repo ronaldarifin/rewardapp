d4c515c8b4dc714bc44f01730d94f78723068d12
ronaldarifin <71082114+ronaldarifin@users.noreply.github.com>
2022-10-15 21:28:44 -0700

    Latest code

--- src/routes/Configure.js
+++ src/routes/Configure.js
@@ -1,64 +1,88 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect} from 'react'
 import { Button, Form, Card, Table} from 'react-bootstrap'
 
 const Configure = () => {
   const [prizes, setPrizes] = useState([{
-    id:1,
     name: "rest 10 min",
-    value: 20,
+    weight: 20,
   },{
-    id:2,
     name: "rest 20min",
-    value: 10,
+    weight: 10,
   },{
-    id:3,
     name: "20 pushups",
-    value: 40,
+    weight: 40,
   }]);
+  //form individual states
+  const [prizeName,setPrizeName] = useState("");
+  const [weight,setWeight] = useState(0);
   
 //TOFIX: make so that the rendering process have no error
-  let renderTable = 
-    <Table>
-      <thead>
+  let renderTable = (
+    <table>
+      {/* <thead>
         <tr>
           {Object.keys(prizes[0]).map((title,key) => <th key={key}>{title}</th>)}
         </tr>
-      </thead>
+      </thead> */}
       <tbody>
         {prizes.map((prizes,key) => {
           return(
             <tr key={key}>
-              <td>{key}</td>
               <td>{prizes.name}</td>
               <td>{prizes.value}</td>
             </tr>
           )
         })}
       </tbody>
-    </Table>
+    </table>
+  )
+
+  const handlePrizeName = (e) => {
+    setPrizeName(e.target.value);
+  }
+  const handleWeight = (e) => {
+    setWeight(e.target.value);
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      name:prizeName,
+      weight:weight
+    }
+    // debugger;
+    setPrizes((prevState) => {
+      [...prevState,data];
+    });
+    console.log(data);
+    // debugger;
+  }
+  useEffect(() => {
+    console.log(prizes)
+  }, [prizes])
+  
     
   return (
     <>
       {/* make into component */}
       <Card body>
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <Form.Group controlId="field1">
             <Form.Label>Reward Name</Form.Label>
-            <Form.Control placeholder="Enter Reward Name" />
+            <Form.Control placeholder="Enter Reward Name" onChange={handlePrizeName}/>
           </Form.Group>
         </Form>
         <Form>
           <Form.Group controlId="Weight">
             <Form.Label>Weight</Form.Label>
-            <Form.Control placeholder="Enter " />
-            <Form.Text className="text-muted">
+            <Form.Control placeholder="Enter Weight " onChange={handleWeight}/>
+            <Form.Text className="text-muted" >
               The weight will be used to calculate the percentage of reward.
             </Form.Text>
           </Form.Group>
+          <Button onClick={(e) => handleSubmit(e)}>
+            Submit
+          </Button>
         </Form>
-        <Button type="submit">
-          Submit
-        </Button>
       </Card>
 
       {/* resulting table */}
