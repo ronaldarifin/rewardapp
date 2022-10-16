e4e26ca10f7abf29f7e15a69b634677935dc7f80
Nathaniel Iskandar <nathanielri22@gmail.com>
2022-10-16 02:43:39 -0700

    test

--- src/routes/Configure.js
+++ src/routes/Configure.js
@@ -1,78 +1,99 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect} from 'react'
 import { Button, Form, Card, Table} from 'react-bootstrap'
 
+
 const Configure = () => {
-  // to understand this. We basicallly want to have a name. In our first proto.
-  // the we need to compute the percentage of it getting the reward.
   const [prizes, setPrizes] = useState([{
-    // id:1,
     name: "rest 10 min",
-    value: 20,
+    weight: 20,
   },{
-    // id:2,
     name: "rest 20min",
-    value: 10,
+    weight: 10,
   },{
-    // id:3,
     name: "20 pushups",
-    value: 40,
+    weight: 40,
   }]);
-  const [viewablePrizes, setViewablePrizes] = useState(false)
-  const [weight, setWeight] = useState("")
-  const [name, setName] = useState("")
+  //form individual states
+  const [prizeName,setPrizeName] = useState("");
+  const [weight,setWeight] = useState(0);
+  
+
+
+
+
+  
 //TOFIX: make so that the rendering process have no error
-  let renderTable = 
-    <Table>
-      <thead>
+  let renderTable = (
+    <table>
+      {/* <thead>
         <tr>
           {Object.keys(prizes[0]).map((title,key) => <th key={key}>{title}</th>)}
         </tr>
-      </thead>
+      </thead> */}
       <tbody>
         {prizes.map((prizes,key) => {
           return(
             <tr key={key}>
-              <td>{key + 1}</td>
               <td>{prizes.name}</td>
               <td>{prizes.value}</td>
             </tr>
           )
         })}
       </tbody>
-    </Table>
+    </table>
+  )
+
+  const handlePrizeName = (e) => {
+    setPrizeName(e.target.value);
+  }
+  const handleWeight = (e) => {
+    setWeight(e.target.value);
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      name:prizeName,
+      weight:weight
+    }
+    // debugger;
+    setPrizes((prevState) => {
+      [...prevState,data];
+    });
+    console.log(data);
+    // debugger;
+  }
+  useEffect(() => {
+    console.log(prizes)
+  }, [prizes])
+  
     
   return (
     <>
       {/* make into component */}
       <Card body>
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <Form.Group controlId="field1">
             <Form.Label>Reward Name</Form.Label>
-            <Form.Control placeholder="Enter Reward Name" onChange={(e) => setName(e.target.value)}/>
-            {/* <input onChange={(e) => setName(e.target.value)}/> */}
+            <Form.Control placeholder="Enter Reward Name" onChange={handlePrizeName}/>
           </Form.Group>
         </Form>
         <Form>
           <Form.Group controlId="Weight">
             <Form.Label>Weight</Form.Label>
-            <Form.Control placeholder="Enter " onChange={(e) => setWeight(e.target.value)}/>
-            {/* <input onChange={(e) => setWeight(e.target.value)}/> */}
-            <Form.Text className="text-muted">
+            <Form.Control placeholder="Enter Weight " onChange={handleWeight}/>
+            <Form.Text className="text-muted" >
               The weight will be used to calculate the percentage of reward.
             </Form.Text>
           </Form.Group>
+          <Button onClick={(e) => handleSubmit(e)}>
+            Submit
+          </Button>
         </Form>
-        <Button type="submit" onClick={() => {
-          console.log(weight,name)
-          setPrizes([...prizes,{name,value:weight}]);setViewablePrizes(true);}}>
-        {/* [...prevState,{weight, name}] */}
-          Submit
-        </Button>
       </Card>
 
       {/* resulting table */}
       {renderTable}
-      {/* {viewablePrizes && <div>There is a prize</div>} */}
+      
     </>
   )
 }
